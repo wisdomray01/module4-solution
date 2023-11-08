23711283876e63504f57457262f1f7fa667da8c0
wisdomray01 <wisdomray25@gmail.com>
2023-11-08 12:32:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-byeSpeaker.sayBye();
-helloSpeaker.sayHello();
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        helloSpeaker.speak(names[i]);
+      } 
+      else {
+        byeSpeaker.speak(names[i]);
+      }
+    }
+  })();
